5322dc628d7f1ee093002a87effc87ed90dbbc6c
memto <dvn0zzz@gmail.com>
2021-12-28 12:02:06 +0700

    setup default Datasource/Schema

--- src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
@@ -1,26 +1,34 @@
 package com.example.springsecuritydemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("blah")
-                .password("blah")
-                .roles("USER")
-                .and()
-                .withUser("admin")
-                .password("admin")
-                .roles("ADMIN");
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .withDefaultSchema()  // Thanks to spring boot => h2database
+            .withUser(User.withUsername("user")
+                        .password("user")
+                        .roles("USER"))
+            .withUser(User.withUsername("admin")
+                    .password("admin")
+                    .roles("ADMIN"));
     }
 
     @Bean
@@ -36,8 +44,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/", "static/css/**", "static/js/**").permitAll()
                 // type of login
-                .and()
-                .formLogin();
-
+                .and().formLogin();
     }
 }
