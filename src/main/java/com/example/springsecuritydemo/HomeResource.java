8a577bfd03d437533b3ce84bc6eef008b4df0851
memto <dvn0zzz@gmail.com>
2021-12-28 11:07:47 +0700

    config authorization

--- src/main/java/com/example/springsecuritydemo/HomeResource.java
+++ src/main/java/com/example/springsecuritydemo/HomeResource.java
@@ -9,4 +9,12 @@ public class HomeResource {
     public String home() {
         return "<h1>Welcome</h1>";
     }
+    @GetMapping("/user")
+    public String user() {
+        return "<h1>Welcome user</h1>";
+    }
+    @GetMapping("/admin")
+    public String admin() {
+        return "<h1>Welcome admin</h1>";
+    }
 }
--- src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
+++ src/main/java/com/example/springsecuritydemo/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.springsecuritydemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -9,16 +10,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("blah")
                 .password("blah")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                // access rights
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/", "static/css/**", "static/js/**").permitAll()
+                // type of login
+                .and()
+                .formLogin();
+
     }
 }
